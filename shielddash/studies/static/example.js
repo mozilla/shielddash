214082a9ca64f8165fb9de8ff92bc4b796a340c2
Rob Hudson <robhudson@mozilla.com>
2016-05-17 14:09:56 -0700

    Open up example studies api call

--- shielddash/studies/static/example.js
+++ shielddash/studies/static/example.js
@@ -1,16 +1,16 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     var id_token = googleUser.getAuthResponse().id_token;
-    var targetNode = document.getElementById("api-data");
+    var targetNode = document.getElementById('api-data');
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    window.fetch(
-        "/studies/?start=20160101&end=20160201",
-        {method: "GET",
-         headers: {'Authorization': 'JWT ' + id_token}}
-    ).then(
+    window.fetch('/studies/', {
+        method: "GET",
+        headers: {'Authorization': 'JWT ' + id_token}
+    }).then(
         res => res.text()).then(
-        txt => targetNode.innerHTML = "hello " + txt);
+        txt => targetNode.innerHTML = txt
+    );
 }
