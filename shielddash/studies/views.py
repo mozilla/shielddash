8520c64e2b0bdaaf9c8d6e76e839826ebec8cc87
Jannis Leidel <jannis@leidel.info>
2016-05-17 20:38:24 +0200

    Close cursor once SQL query is done.

--- shielddash/studies/views.py
+++ shielddash/studies/views.py
@@ -25,27 +25,27 @@ class StudyListView(ListAPIView):
 @api_view(['GET'])
 def study_detail(request, study_id):
     study = get_object_or_404(Study, pk=study_id)
-    cursor = connection.cursor()
-    sql = """
-        SELECT
-            channel,
-            variation,
-            SUM(completed::int) AS completed,
-            SUM(ineligible::int) AS ineligible,
-            SUM(installed::int) AS installed,
-            SUM(left_study::int) AS left_study,
-            SUM(seen1::int) AS seen1,
-            SUM(seen2::int) AS seen2,
-            SUM(seen3::int) AS seen3,
-            SUM(seen7::int) AS seen7
-        FROM studies_state
-        WHERE study_id=%s
-        GROUP BY channel, variation
-        ORDER BY channel, variation
-    """
-    cursor.execute(sql, [study.id])
-    columns = [col[0] for col in cursor.description]
-    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    with connection.cursor() as cursor:
+        sql = """
+            SELECT
+                channel,
+                variation,
+                SUM(completed::int) AS completed,
+                SUM(ineligible::int) AS ineligible,
+                SUM(installed::int) AS installed,
+                SUM(left_study::int) AS left_study,
+                SUM(seen1::int) AS seen1,
+                SUM(seen2::int) AS seen2,
+                SUM(seen3::int) AS seen3,
+                SUM(seen7::int) AS seen7
+            FROM studies_state
+            WHERE study_id=%s
+            GROUP BY channel, variation
+            ORDER BY channel, variation
+        """
+        cursor.execute(sql, [study.id])
+        columns = [col[0] for col in cursor.description]
+        rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
 
     data = {
         'study': study.name,
