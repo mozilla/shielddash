60815f30ea2df3c65b4a881bee94031d9b6f4b4b
Davor Spasovski <davor@spasovski.net>
2016-05-25 23:55:42 -0400

    some style cleanup and target unnesting

--- shielddash/core/static/js/app/components/views/main-heading.js
+++ shielddash/core/static/js/app/components/views/main-heading.js
@@ -4,7 +4,11 @@ import {Link} from 'react-router';
 
 export default function(props) {
   if (props.isLinked) {
-    return (<h1><Link to="/">SHIELD Dashboard</Link></h1>);
+    return (
+      <h1>
+        <Link className="primary-header-link" to="/">SHIELD Dashboard</Link>
+      </h1>
+    );
   }
   return (<h1>SHIELD Dashboard</h1>);
 }
