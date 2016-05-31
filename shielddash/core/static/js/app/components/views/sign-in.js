26aa80b39a5c12695b2f80bf9d4a2c267912da8a
John Karahalis <john.karahalis@gmail.com>
2016-05-31 15:40:06 -0400

    Fix #29: Embed a "Sign In" button with notice

--- shielddash/core/static/js/app/components/views/sign-in.js
+++ shielddash/core/static/js/app/components/views/sign-in.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoginButtonContainer from '../containers/login-button-container';
 
 
 export default function(props) {
   return (
     <article className="sign-in-content">
-      <h1>Please sign in to continue.</h1>
+      <h1>Please <LoginButtonContainer /> to continue.</h1>
     </article>
   );
 }
